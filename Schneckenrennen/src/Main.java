bbdb2cda81eec7cf001f6341f4f75716e2088b1b
Friedrich.Raoul <Friedrich.Raoul@DESKTOP-GQ1I3O9.2631.skool.net>
2018-10-22 14:47:30 +0200

    Klasse Rennen angelegt

--- Schneckenrennen/src/Main.java
+++ Schneckenrennen/src/Main.java
@@ -1,11 +1,11 @@
 
-public class Schneckenrennen {
+public class Main {
 
 	public static void main(String[] args) {
 		
 		Rennschnecke Schneggrich1 = new Rennschnecke("Schneggberd", "Glitscho",90);
-		Rennschnecke Schneggrich2 = new Rennschnecke("Schneggberd2", "Ultimato", 6);
-		Rennschnecke Schneggrich3 = new Rennschnecke("Schleima", "Nihaaa", 3331);
+		Rennschnecke Schneggrich2 = new Rennschnecke("Schneggberd2", "Kier", 6);
+		Rennschnecke Schneggrich3 = new Rennschnecke("Schleima", "Niha", 3331);
 		Schneggrich1.krieche();
 		System.out.println(Schneggrich1);
 		Schneggrich2.krieche();
